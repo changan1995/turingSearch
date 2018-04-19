5e74f4a4d78fb38bfa5e89567cad38656dbbafb4
changan1995 <changanw@seas.upenn.edu>
2018-04-18 23:16:54 -0400

    change directory to lowerletter

---
+++ src/main/java/edu/upenn/cis555/searchengine/App.java
@@ -0,0 +1,13 @@
+// package edu.upenn.cis555.searchengine;
+
+// /**
+//  * Hello world!
+//  *
+//  */
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//     }
+// }
---
+++ src/main/java/edu/upenn/cis555/searchengine/crawler/storage/Doc.java
@@ -0,0 +1,80 @@
+package edu.upenn.cis555.searchengine.crawler.storage;
+
+
+import org.w3c.dom.Document;
+
+import edu.upenn.cis555.searchengine.crawler.Utilities;
+import edu.upenn.cis555.searchengine.crawler.info.URLInfo;
+// import edu.upenn.cis455.hw1.HttpServer;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+@Entity
+public class Doc {
+	
+	@PrimaryKey
+	private String url;
+		 
+	private String content;
+	private String docType;
+	private long docLength;
+    private String crawledDate;
+    private String absHost;
+    private String lastModified;
+	
+    public Doc() {}
+    
+	public Doc(String url, String content, String docType, long docLength, String lastModified) {
+        URLInfo temp = new URLInfo(url);
+        this.absHost = temp.getAbsoluteRoot();
+		this.url = url; 
+        this.content = content;
+		this.docType = docType;
+		this.docLength = docLength;
+		this.crawledDate = Utilities.convertTime(System.currentTimeMillis());
+        if(lastModified==null){
+            this.lastModified=Utilities.convertTime(0);
+        }else{
+            this.lastModified = lastModified;
+        }
+	}
+	
+	public String getUrl() { return this.url; }
+	public String getDocType() { return this.docType; }
+	public long getDocLength() { return this.docLength; }
+    public String getcrawledDate() { return this.crawledDate; }
+    public String getLastModified() {return this.lastModified;}
+	
+	public void setUrl(String url) {
+		this.url = url;
+    }
+    
+	public void setDocument(String content, String docType) {
+		this.content = content;
+		this.docType = docType;
+	}
+    
+    public String getContent(){
+        return this.content;
+    }
+
+	public void setcrawledDate() {
+		this.crawledDate = Utilities.convertTime(System.currentTimeMillis());
+	}
+
+	/**
+	 * @return the absHost
+	 */
+	public String getAbsHost() {
+		return absHost;
+	}
+
+	/**
+	 * @param absHost the absHost to set
+	 */
+	public void setAbsHost(String absHost) {
+		this.absHost = absHost;
+	}
+	
+}
