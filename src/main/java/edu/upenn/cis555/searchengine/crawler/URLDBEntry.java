20e4cf18839d253deb59d4c78cf4a91146db7f70
changan1995 <changan1995@changan1995deMacBook-Pro.local>
2018-04-19 21:39:54 -0400

    s3 test

---
+++ src/main/java/edu/upenn/cis555/searchengine/crawler/URLDBEntry.java
@@ -0,0 +1,23 @@
+package edu.upenn.cis555.searchengine.crawler;
+
+import java.util.Set;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import com.amazonaws.services.dynamodbv2.datamodeling.S3Link;
+
+@DynamoDBTable(tableName = "user-table")
+public class URLDBEntry {
+    private String urlString;
+    private S3Link contentLink;
+    private Set outLink;
+
+    @DynamoDBHashKey
+    public String getUrl() {return urlString;}
+    public void setUrl(String urlString) {this.urlString = urlString;}
+
+    @DynamoDBAttribute
+    public S3Link getContentLink() {return contentLink;}
+    public void setConentLink(S3Link content) {this.contentLink = content;}
+
+    
+}
