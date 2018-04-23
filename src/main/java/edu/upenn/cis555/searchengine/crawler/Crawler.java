ff3ce1c1f118e0630411aa24290ea54a9e1ce0f3
changan1995 <changan1995@changan1995deMacBook-Pro.local>
2018-04-23 04:20:23 -0400

    limit size 100KB

--- src/main/java/edu/upenn/cis555/searchengine/crawler/Crawler.java
+++ src/main/java/edu/upenn/cis555/searchengine/crawler/Crawler.java
@@ -35,7 +35,7 @@ public class Crawler {
 	public static Map<String, RobotsTxtInfo> robotLst;// TODO:concurrent handle
 	public static int crawledNum = 250000;
 	public static BloomFilter<CharSequence> bl;
-	public static int maxFileSize = 1 * 1024 * 1024;
+	public static int maxFileSize = 100 * 1024;
 	
 	public static AtomicInteger num = new AtomicInteger(0);
 
