268325fbd4f708ab88b997bda23b629cdfc8d9ec
changan1995 <changan1995@changan1995deMacBook-Pro.local>
2018-04-28 00:26:38 -0400

    limit queue size

--- src/main/java/edu/upenn/cis555/searchengine/crawler/URLFrontier.java
+++ src/main/java/edu/upenn/cis555/searchengine/crawler/URLFrontier.java
@@ -48,7 +48,7 @@ public class URLFrontier {
 	// ConcurrentHashMap<String, Integer> delayCache;
 	// private long initTime=System.currentTimeMillis();
 	// int hostSize = numThreads *15;
-	private int maxQueueNum = 200; 
+	private int maxQueueNum = 50; 
 	private int maxHostNum;
 	
 	// int upperLimit;
