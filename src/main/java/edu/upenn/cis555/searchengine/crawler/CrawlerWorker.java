f068688fd402ec41a0a36efc88a2d892d5e9579c
changan1995 <changan1995@changan1995deMacBook-Pro.local>
2018-04-28 03:10:18 -0400

    updated

--- src/main/java/edu/upenn/cis555/searchengine/crawler/CrawlerWorker.java
+++ src/main/java/edu/upenn/cis555/searchengine/crawler/CrawlerWorker.java
@@ -55,7 +55,7 @@ public class CrawlerWorker implements Runnable {
     private URLFrontier frontier;
 	private URLDistributor distributor;
 	public static Pattern pattern = Pattern.compile("^http[s]?://.*(facebook|google|twitter|amazon|linkedin|pornhub|weibo|instagram|blogspot|tumblr)\\.com.*");
-	public static Pattern blackLst = Pattern.compile("cn|.39.|163|nuomi|academia|getithalfoff|cn|search|gril|porn|sex|create|subscribe|jp|hao123|qq|jd|shanghai|beijing|china|360|hao315|leju|kankan|pussy|blog|fangjia|fangzi|cheshi|fuck|fbi");
+	public static Pattern blackLst = Pattern.compile("cn|.39.|163|nuomi|glassdoor|fr.|academia|getithalfoff|cn|search|gril|porn|sex|create|subscribe|jp|hao123|qq|jd|shanghai|beijing|china|360|hao315|leju|kankan|pussy|blog|fangjia|fangzi|cheshi|fuck|fbi");
     
     public CrawlerWorker(int id, int crawledNum, URLFrontier frontier){
         this.id =id;
